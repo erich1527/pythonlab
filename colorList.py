d4e9667cab37bda89d8349e571a2d7f03881912d
Eric Hendricks <eric.hendricks@ascendlearning.com>
2018-07-27 17:53:57 -0500

    renamed breakCount, added colorList

---
+++ colorList.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.7
+# 
+
+colors = ['blue', 'green', 'red', 'purple']
+
+for color in colors:
+    if color == 'blue':
+        continue
+    elif color == 'red':
+        break
+    print(color)
