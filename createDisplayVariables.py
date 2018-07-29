24cc7ddfa5e2deb23990c364e84764306f3ddd57
Eric Hendricks <eric.hendricks@ascendlearning.com>
2018-07-29 12:47:59 -0500

    Continuing training!

---
+++ createDisplayVariables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.7
+#Write a python script that sets the a list of variables
+
+first_name = "James"
+last_name = "Conway"
+age =   "41"
+birth_date = "April 4th, 2063"
+
+print(f"My name is {first_name} {last_name}.")
+print(f"I was born on {birth_date} and am {age} years old.")
---
+++ workingWithIf.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3.7
+# Create a script with a single variable contianing a dict
+
+user = { 'admin': True, 'active': True, 'name': 'Eric' }
+
