202344e54eb46ceb1c5948ccf32b0f14740f8410
Eric Hendricks <eric.hendricks@ascendlearning.com>
2018-07-29 14:36:22 -0500

    Renamed file, removed execute, completed lab

--- workingWithIf.py
+++ workingWithIf.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3.7
 # Create a script with a single variable contianing a dict
 
-user = { 'admin': True, 'active': True, 'name': 'Eric' }
+user = { 'admin': True, 'active': False, 'name': 'Eric' }
+prefix = ""
 
+if user['admin'] and user['active']:
+    prefix = "Active - (ADMIN) "
+elif user['admin']:
+    prefix = "(ADMIN) "
+elif user['active']:
+    prefix = "ACTIVE - "
+
+print(prefix + user['name'])
